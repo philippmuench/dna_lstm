f0dc31f16660d8e1cdd6c4a097d47d0300f74fba
philippmuench <philipp.muench@helmholtz-hzi.de>
2017-07-23 19:59:17 +0200

    add Lambda layer needen for visualization

--- train.py
+++ train.py
@@ -1,5 +1,8 @@
-import pydot
-import graphviz
+import tensorflow as tf
+import theano
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from keras.layers import Dense, Dropout, LSTM, Embedding, Activation, Lambda
 from keras.engine import Input, Model, InputSpec
 from keras.preprocessing.sequence import pad_sequences
@@ -8,23 +11,31 @@ from keras.utils.data_utils import get_file
 from keras.models import Sequential
 from keras.callbacks import ModelCheckpoint
 from keras import backend as K
+from keras.preprocessing import sequence
 from keras.models import model_from_json
 from kerastoolbox.visu import plot_weights
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import theano
-import tensorflow as tf
+import os
+import pydot
+import graphviz
 
-epochs = 10
+EPCOHS = 3 #  an arbitrary cutoff, generally defined as "one pass over the entire dataset", used to separate training into distinct phases, which is useful for logging and periodic evaluation.
+BATCH_SIZE = 256 # a set of N samples. The samples in a batch are processed independently, in parallel. If training, a batch results in only one update to the model.
+INPUT_DIM = 4 # a vocabulary of 4 words in case of fnn sequence
+OUTPUT_DIM = 512
+RNN_HIDDEN_DIM = 128
+DROPOUT_RATIO = 0.2 # proportion of neurones not used for training
+MAXLEN = 200 # cuts text after number of these characters in pad_sequences
 
-input_file = 'input_small.csv'
+checkpoint_dir ='checkpoints'
+os.path.exists(checkpoint_dir)
+
+input_file = 'input_med.csv'
 
 def letter_to_index(letter):
     _alphabet = 'ATGC'
     return next((i for i, _letter in enumerate(_alphabet) if _letter == letter), None)
 
-def load_data(test_split = 0.2):
+def load_data(test_split = 0.2, maxlen = MAXLEN):
     print ('Loading data...')
     df = pd.read_csv(input_file)
     df['sequence'] = df['sequence'].apply(lambda x: [int(letter_to_index(e)) for e in x])
@@ -34,20 +45,22 @@ def load_data(test_split = 0.2):
     y_train = np.array(df['target'].values[:train_size])
     X_test = np.array(df['sequence'].values[train_size:])
     y_test = np.array(df['target'].values[train_size:])
-    return pad_sequences(X_train), y_train, pad_sequences(X_test), y_test
+    print('Average train sequence length: {}'.format(np.mean(list(map(len, X_train)), dtype=int)))
+    print('Average test sequence length: {}'.format(np.mean(list(map(len, X_test)), dtype=int)))
+    return pad_sequences(X_train, maxlen=maxlen), y_train, pad_sequences(X_test, maxlen=maxlen), y_test
 
-def create_model(input_length):
+def create_model(input_length, rnn_hidden_dim = RNN_HIDDEN_DIM, output_dim = OUTPUT_DIM, input_dim = INPUT_DIM, dropout = DROPOUT_RATIO):
     print ('Creating model...')
     model = Sequential()
-    model.add(Embedding(input_dim = 200, output_dim = 150, input_length = input_length))
-    model.add(LSTM(output_dim=128, activation='sigmoid', inner_activation='hard_sigmoid', return_sequences=True))
-    model.add(Dropout(0.2))
-    model.add(LSTM(output_dim=128, activation='sigmoid', inner_activation='hard_sigmoid'))
-    model.add(Dropout(0.2))
-    model.add(Dense(1, activation='sigmoid'))
+    # we start off with an efficient embedding layer which maps our vocab indices into embedding_dims dimensions
+    model.add(Embedding(input_dim = INPUT_DIM, output_dim = output_dim, input_length = input_length, name='embedding_layer'))
+    model.add(LSTM(units=rnn_hidden_dim, dropout = dropout, recurrent_dropout= dropout, return_sequences=True, name='recurrent_layer'))
+    model.add(Lambda(lambda x: x[:, -1, :], output_shape=(rnn_hidden_dim, ), name='last_step_layer'))
+    # We project onto a single unit output layer, and squash it with a sigmoid:
+    model.add(Dense(1, activation='sigmoid', name='output_layer'))
     print ('Compiling...')
     model.compile(loss='binary_crossentropy',
-                  optimizer='rmsprop',
+                  optimizer='adam',
                   metrics=['accuracy'])
     return model
 
@@ -70,32 +83,32 @@ def create_plots(history):
     plt.savefig('loss.png')
     plt.clf()
 
-X_train, y_train, X_test, y_test = load_data()
-model = create_model(len(X_train[0]))
+if __name__ == '__main__':
+    # train
+    X_train, y_train, X_test, y_test = load_data()
+    model = create_model(len(X_train[0]))
 
-# checkpoint
-filepath="checkpoints/weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
-checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=0, save_best_only=True, mode='max')
-callbacks_list = [checkpoint]
+    # checkpoint
+    filepath= checkpoint_dir + "/weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
+    checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=0, save_best_only=True, mode='max')
+    callbacks_list = [checkpoint]
 
+    print ('Fitting model...')
+    history = model.fit(X_train, y_train, batch_size=BATCH_SIZE, epochs=EPCOHS, callbacks=callbacks_list, validation_split = 0.1, verbose = 1)
 
-print ('Fitting model...')
-history = model.fit(X_train, y_train, batch_size=100, epochs=epochs, callbacks=callbacks_list, validation_split = 0.1, verbose = 1)
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights("model.h5")
+    print("Saved model to disk")
 
-# serialize model to JSON
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
-    json_file.write(model_json)
-# serialize weights to HDF5
-model.save_weights("model.h5")
-print("Saved model to disk")
+    create_plots(history)
+    plot_model(model, to_file='model.png')
 
-create_plots(history)
-plot_model(model, to_file='model.png')
-
-# summarize history for loss
-score, acc = model.evaluate(X_test, y_test, batch_size=1)
-
-print('Test score:', score)
-print('Test accuracy:', acc)
+    # summarize history for loss
+    score, acc = model.evaluate(X_test, y_test, batch_size=BATCH_SIZE)
+    print('Test score:', score)
+    print('Test accuracy:', acc)
 
